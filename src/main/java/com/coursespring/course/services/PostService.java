eb303610a60b3bd058580e38a4243cff789a0e24
Manasses <manmiui2001@gmail.com>
2020-07-15 17:50:02 -0300

    Getting post by ID

---
+++ src/main/java/com/coursespring/course/services/PostService.java
@@ -0,0 +1,23 @@
+package com.coursespring.course.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coursespring.course.domain.Post;
+import com.coursespring.course.repository.PostRepository;
+import com.coursespring.course.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}

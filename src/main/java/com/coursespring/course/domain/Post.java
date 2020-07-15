ebff0123e38c0b914249e396808e986da9fae2f4
Manasses <manmiui2001@gmail.com>
2020-07-15 17:59:07 -0300

    Adding comments to posts

--- src/main/java/com/coursespring/course/domain/Post.java
+++ src/main/java/com/coursespring/course/domain/Post.java
@@ -1,12 +1,15 @@
 package com.coursespring.course.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.coursespring.course.dto.AuthorDTO;
+import com.coursespring.course.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	private AuthorDTO author;
 
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override

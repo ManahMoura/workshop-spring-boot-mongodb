7c8e5c989cb18b4d708b73b219ddf7399856733e
Manasses <manmiui2001@gmail.com>
2020-07-15 15:43:00 -0300

    Getting a user by ID

---
+++ src/main/java/com/coursespring/course/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.coursespring.course.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.coursespring.course.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	// StandardError should return Instant, Integer, String, String, String, but had an unsolved problem...
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Not found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
---
+++ src/main/java/com/coursespring/course/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.coursespring.course.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
